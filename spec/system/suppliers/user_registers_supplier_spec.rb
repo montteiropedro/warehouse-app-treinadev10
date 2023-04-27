22e442aed9cdac34f3c0a7a3d9cc2941d3de8059
Pedro Monteiro <montteiropedro@gmail.com>
2023-04-27 11:38:06 -0300

    tests: user registers supplier

---
+++ spec/system/suppliers/user_registers_supplier_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+
+describe 'User registers a supplier' do
+  it 'from suppliers page' do
+    # Arrange (pass)
+    
+    # Act
+    visit root_path
+    click_on 'Fornecedores'
+    click_on 'Cadastrar Fornecedor'
+
+    # Assert
+    expect(current_path).to eq new_supplier_path
+
+    expect(page).to have_field 'Nome Fantasia'
+    expect(page).to have_field 'Razão Social'
+    expect(page).to have_field 'CNPJ'
+    expect(page).to have_field 'Endereço'
+    expect(page).to have_field 'Cidade'
+    expect(page).to have_field 'Estado'
+    expect(page).to have_field 'E-mail'
+  end
+
+  it 'should be successful' do
+    # Arrange (pass)
+
+    # Act
+    visit root_path
+    click_on 'Fornecedores'
+    click_on 'Cadastrar Fornecedor'
+
+    fill_in 'Nome Fantasia', with: 'ACME'
+    fill_in 'Razão Social', with: 'ACME LTDA'
+    fill_in 'CNPJ', with: '43447216000102'
+    fill_in 'Endereço', with: 'Av das Palmas, 100'
+    fill_in 'Cidade', with: 'Bauru'
+    fill_in 'Estado', with: 'SP'
+    fill_in 'E-mail', with: 'contato@acme.com'
+    click_on 'Enviar'
+
+    # Assert
+    expect(page).to have_content 'Fornecedor cadastrado com sucesso.'
+  end
+
+  it 'should be unsuccessful with incomplete data' do
+    # Arrange (pass)
+
+    # Act
+    visit root_path
+    click_on 'Fornecedores'
+    click_on 'Cadastrar Fornecedor'
+    
+    fill_in 'Nome Fantasia', with: ''
+    fill_in 'Razão Social', with: ''
+    fill_in 'CNPJ', with: ''
+    fill_in 'Endereço', with: ''
+    fill_in 'Cidade', with: ''
+    fill_in 'Estado', with: ''
+    fill_in 'E-mail', with: ''
+    click_on 'Enviar'
+
+    # Assert
+    expect(current_path).to eq suppliers_path
+
+    expect(page).to have_content 'Falha ao cadastrar o fornecedor.'
+    
+    expect(page).to have_content 'Nome Fantasia não pode ficar em branco'
+    expect(page).to have_content 'Razão Social não pode ficar em branco'
+    expect(page).to have_content 'CNPJ não pode ficar em branco'
+    expect(page).to have_content 'Endereço não pode ficar em branco'
+    expect(page).to have_content 'Cidade não pode ficar em branco'
+    expect(page).to have_content 'Estado não pode ficar em branco'
+    expect(page).to have_content 'E-mail não pode ficar em branco'
+  end
+end
