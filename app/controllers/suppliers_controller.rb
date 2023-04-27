36ebd03fc26d135bb3cc46bb0ded3e16dcb51a0d
Pedro Monteiro <montteiropedro@gmail.com>
2023-04-27 13:09:46 -0300

    add: Supplier Routes (index, show, new, create, edit, update), Model + Migrations, Controller Actions (index, show, new, create, edit, update), Supplier Views (index, show, new, edit)

---
+++ app/controllers/suppliers_controller.rb
@@ -0,0 +1,29 @@
+class SuppliersController < ApplicationController
+  before_action :set_supplier, only: [:show, :edit, :update]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def set_supplier
+  end
+
+  def supplier_params
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'home#index'
   resources :warehouses, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :suppliers, only: [:index, :show, :new, :create, :edit, :update]
 end
