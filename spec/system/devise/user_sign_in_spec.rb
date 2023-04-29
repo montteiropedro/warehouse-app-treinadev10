1210febaa05ba32f1b7e616b902b3311f1353f10
Pedro Monteiro <montteiropedro@gmail.com>
2023-04-29 11:47:18 -0300

    changes: devise views, devise session and registration tests

--- spec/system/devise/user_sign_in_spec.rb
+++ spec/system/devise/user_sign_in_spec.rb
@@ -1,8 +1,24 @@
 require 'rails_helper'
 
-describe 'User session with devise' do
-  context 'Sign in' do
-    it 'and should be successful with correct data' do
+describe 'User session (devise)' do
+  it 'from navigation menu' do
+    # Arrange (pass)
+
+    # Act
+    visit root_path
+    click_on 'Entrar'
+
+    # Assert
+    expect(current_path).to eq new_user_session_path
+
+    within('form') do
+      expect(page).to have_field 'E-mail'
+      expect(page).to have_field 'Senha'
+    end
+  end
+
+  context 'sign in' do
+    it 'should be successful with correct data' do
       # Arrange
       User.create!(name: 'John Doe', email: 'john@email.com', password: 'password123')
 
@@ -27,7 +43,7 @@ describe 'User session with devise' do
       end
     end
 
-    it 'and should be unsuccessful with incorrect data' do
+    it 'should be unsuccessful with incorrect data' do
       # Arrange
       User.create!(name: 'John Doe', email: 'john@email.com', password: 'password123')
 
@@ -51,10 +67,35 @@ describe 'User session with devise' do
         expect(page).to have_link 'Entrar'
       end
     end
+
+    it 'should be unsuccessful with incomplete data' do
+      # Arrange
+      User.create!(name: 'John Doe', email: 'john@email.com', password: 'password123')
+
+      # Act
+      visit root_path
+      click_on 'Entrar'
+
+      within('form') do
+        fill_in 'E-mail', with: ''
+        fill_in 'Senha', with: ''
+        click_on 'Entrar'
+      end
+
+      # Assert
+      expect(page).to have_content 'E-mail ou senha inválidos.'
+
+      within('nav') do
+        expect(page).not_to have_content 'John Doe'
+        expect(page).not_to have_button 'Sair'
+
+        expect(page).to have_link 'Entrar'
+      end
+    end
   end
   
-  context 'Sign out' do
-    it 'and should be successful' do
+  context 'sign out' do
+    it 'should be successful' do
       # Arrange
       User.create!(name: 'John Doe', email: 'john@email.com', password: 'password123')
 
---
+++ spec/system/devise/user_sign_up_spec.rb
@@ -0,0 +1,88 @@
+require 'rails_helper'
+
+describe 'User registration (devise)' do
+  it 'from navigation menu' do
+    # Arrange (pass)
+
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    click_on 'Criar conta'
+
+    # Assert
+    expect(current_path).to eq new_user_registration_path
+
+    within('form') do
+      expect(page).to have_field 'Nome'
+      expect(page).to have_field 'E-mail'
+      expect(page).to have_field 'Senha'
+      expect(page).to have_field 'Confirme sua senha'
+    end
+  end
+
+  it 'should be successful' do
+    # Arrange (pass)
+
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    click_on 'Criar conta'
+
+    within('form') do
+      fill_in 'Nome', with: 'John Doe'
+      fill_in 'E-mail', with: 'john@email.com'
+      fill_in 'Senha', with: 'password123'
+      fill_in 'Confirme sua senha', with: 'password123'
+      click_on 'Cadastrar'
+    end
+
+    # Assert
+    expect(current_path).to eq root_path
+
+    expect(page).to have_content 'Bem vindo! Você realizou seu registro com sucesso.'
+  end
+
+  it 'should be unsuccessful with incorrect data' do
+    # Arrange (pass)
+
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    click_on 'Criar conta'
+
+    within('form') do
+      fill_in 'Nome', with: 'John Doe'
+      fill_in 'E-mail', with: 'john@email.com'
+      fill_in 'Senha', with: 'pass1'
+      fill_in 'Confirme sua senha', with: 'pass1'
+      click_on 'Cadastrar'
+    end
+
+    # Assert
+    expect(current_path).to eq user_registration_path
+
+    expect(page).to have_content 'Não foi possível salvar usuário'
+  end
+
+  it 'should be unsuccessful with incomplete data' do
+    # Arrange (pass)
+
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    click_on 'Criar conta'
+
+    within('form') do
+      fill_in 'Nome', with: ''
+      fill_in 'E-mail', with: ''
+      fill_in 'Senha', with: ''
+      fill_in 'Confirme sua senha', with: ''
+      click_on 'Cadastrar'
+    end
+
+    # Assert
+    expect(current_path).to eq user_registration_path
+
+    expect(page).to have_content 'Não foi possível salvar usuário'
+  end
+end
