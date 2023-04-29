6bb63c88ff4b718773e5029e276735fe43816eb6
Pedro Monteiro <montteiropedro@gmail.com>
2023-04-29 10:28:53 -0300

    changes: Layouts View css; tests: create more system tests for user session (sign in with incorrect data, sign out)

--- spec/system/user_sign_in_spec.rb
+++ spec/system/user_sign_in_spec.rb
@@ -1,29 +1,84 @@
 require 'rails_helper'
 
-describe 'User sign-in' do
-  it 'and should be successful' do
-    # Arrange
-    User.create!(name: 'John Doe', email: 'john@email.com', password: 'password123')
+describe 'User session with devise' do
+  context 'Sign in' do
+    it 'and should be successful with correct data' do
+      # Arrange
+      User.create!(name: 'John Doe', email: 'john@email.com', password: 'password123')
 
-    # Act
-    visit root_path
-    click_on 'Entrar'
-
-    within('form') do
-      fill_in 'E-mail', with: 'john@email.com'
-      fill_in 'Senha', with: 'password123'
-
+      # Act
+      visit root_path
       click_on 'Entrar'
+
+      within('form') do
+        fill_in 'E-mail', with: 'john@email.com'
+        fill_in 'Senha', with: 'password123'
+        click_on 'Entrar'
+      end
+
+      # Assert
+      expect(page).to have_content 'Login efetuado com sucesso.'
+
+      within('nav') do
+        expect(page).not_to have_link 'Entrar'
+
+        expect(page).to have_content 'John Doe'
+        expect(page).to have_button 'Sair'
+      end
     end
 
-    # Assert
-    expect(page).to have_content 'Login efetuado com sucesso.'
+    it 'and should be unsuccessful with incorrect data' do
+      # Arrange
+      User.create!(name: 'John Doe', email: 'john@email.com', password: 'password123')
 
-    within('nav') do
-      expect(page).not_to have_link 'Entrar'
+      # Act
+      visit root_path
+      click_on 'Entrar'
 
-      expect(page).to have_content 'John Doe'
-      expect(page).to have_link 'Sair'
+      within('form') do
+        fill_in 'E-mail', with: 'wrong_john@email.com'
+        fill_in 'Senha', with: 'wrongpass123'
+        click_on 'Entrar'
+      end
+
+      # Assert
+      expect(page).to have_content 'E-mail ou senha inválidos.'
+
+      within('nav') do
+        expect(page).not_to have_content 'John Doe'
+        expect(page).not_to have_button 'Sair'
+
+        expect(page).to have_link 'Entrar'
+      end
+    end
+  end
+  
+  context 'Sign out' do
+    it 'and should be successful' do
+      # Arrange
+      User.create!(name: 'John Doe', email: 'john@email.com', password: 'password123')
+
+      # Act
+      visit root_path
+      click_on 'Entrar'
+
+      within('form') do
+        fill_in 'E-mail', with: 'john@email.com'
+        fill_in 'Senha', with: 'password123'
+        click_on 'Entrar'
+      end
+
+      click_on 'Sair'
+
+      # Assert
+      expect(page).to have_content 'Logout efetuado com sucesso.'
+
+      within('nav') do
+        expect(page).not_to have_content 'John Doe'
+        expect(page).not_to have_button 'Sair'
+
+        expect(page).to have_link 'Entrar'
+      end
     end
   end
 end
